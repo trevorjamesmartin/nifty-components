282b062e0eb9c4da0dd309ba24bf819b7ec611d8
Trevor Martin <trevor.j.martin@gmail.com>
2020-08-02 17:04:03 -0400

    added MatrixRain component

--- example/src/App.js
+++ example/src/App.js
@@ -1,14 +1,23 @@
 import React from 'react'
-import { ImageSpinner, Example } from 'nifty-components'
+import { ImageSpinner, MatrixRain } from 'nifty-components'
 
 function App() {
   return (
-    <div className='App'>
-      <Example />
+    <div
+      className='App'
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: '5vw'
+      }}
+    >
+      <MatrixRain />
       <ImageSpinner
         src='https://placekitten.com/g/200/300'
         alt='desription'
         customspinner={<h1>TEST!...</h1>}
+        style={{ borderRadius: '1rem', opacity: '77%' }}
       />
     </div>
   )
--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Example from './Example'
 import ImageSpinner from './ImageSpinner'
 import Card from './Card'
 import GithubCards from './GithubCards'
+import MatrixRain from './MatrixRain'
 
-export { Example, ImageSpinner, Card, GithubCards }
+export { Example, ImageSpinner, Card, GithubCards, MatrixRain }
